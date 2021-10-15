9adc04827d40af5cb531da552f1f48f2b6f7af0f
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-10-15 22:15:05 +0700

    19: @Input data

--- src/app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
+++ src/app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Passenger } from './../../models/passenger.interface';
 
 @Component({
   selector: 'passenger-count',
-  templateUrl: './passenger-count.component.html',
-  styleUrls: ['./passenger-count.component.scss']
+  // templateUrl: './passenger-count.component.html',
+  styleUrls: ['./passenger-count.component.scss'],
+  template: `
+  <div>
+  <h3>Airline Passengers</h3>
+  <div>
+  <!-- Total passengers: {{items.length}} -->
+  Total checked in: {{checkedInCount()}} / {{items.length}}
+  </div>
+  </div>
+  `
 })
 export class PassengerCountComponent implements OnInit {
-
+  
+  @Input()
+  items: Passenger[] = [];
+  
   constructor() { }
-
+  
   ngOnInit(): void {
   }
-
+  
+  checkedInCount(): number {
+    
+    if (!this.items) return 0;
+    
+    return this.items
+                  .filter((passenger: Passenger) => passenger.checkedIn)
+                  .length;
+  }
+  
 }
