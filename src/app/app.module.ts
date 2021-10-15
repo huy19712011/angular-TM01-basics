89d0a8703893313d69b6b53a9db426a6f850ccc5
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-10-15 18:22:23 +0700

    15: create Feature Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 
+import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,6 +11,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     CommonModule,
+    PassengerDashboardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
