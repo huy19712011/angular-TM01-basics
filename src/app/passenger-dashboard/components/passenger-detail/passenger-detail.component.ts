7929285da386bafc1790243a2e66203885df5d84
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-10-15 22:45:06 +0700

    19a: dynamic @Input values with *ngFor

--- src/app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
+++ src/app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -1,12 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Passenger } from './../../models/passenger.interface';
 
 @Component({
   selector: 'passenger-detail',
-  templateUrl: './passenger-detail.component.html',
-  styleUrls: ['./passenger-detail.component.scss']
+  // templateUrl: './passenger-detail.component.html',
+  styleUrls: ['./passenger-detail.component.scss'],
+  template: `
+    <div>
+      <span 
+        class="status"
+        [class.checked-in]="detail.checkedIn"
+        >
+      </span>
+      {{detail.fullname}}
+      
+      <div class="date">
+        Check in date: {{detail.checkInDate
+          ? (detail.checkInDate | date: 'y, MMMM d' | uppercase) 
+          : 'Not checked in'}}
+      </div>
+      
+      <div class="children">
+        Children: {{detail.children?.length || 0}}
+      </div>
+    </div>
+
+  `
 })
 export class PassengerDetailComponent implements OnInit {
 
+  @Input()
+  detail!: Passenger;
+  
   constructor() { }
 
   ngOnInit(): void {
--- src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -8,31 +8,16 @@ import { Passenger } from '../../models/passenger.interface';
   styleUrls: ['./passenger-dashboard.component.scss'],
   template: `
     <div>
-      <passenger-count [items]="passengers">
-        
+      <passenger-count 
+        [items]="passengers"
+        >
       </passenger-count>
-      <passenger-detail></passenger-detail>
-      <h3>Airline Passengers</h3>
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index">
-          <span 
-            class="status"
-            [class.checked-in]="passenger.checkedIn"
-            >
-          </span>
-          {{i}}: {{passenger.fullname}}
-          <p>{{passenger | json}}</p>
-          <div class="date">
-            Check in date: {{passenger.checkInDate
-              ? (passenger.checkInDate | date: 'y, MMMM d' | uppercase) 
-              : 'Not checked in'}}
-          </div>
-          <div>
-            Children: {{passenger.children?.length || 0}}
-          </div>
-        </li>
-      </ul>
-
+      
+      <passenger-detail 
+        *ngFor="let passenger of passengers"
+        [detail]="passenger"
+        >
+      </passenger-detail>
     </div>
   `
 })
