a4cadadda864adcb6cc582b7f7ceb3116b7e0de1
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-10-20 21:52:22 +0700

    34: Form validation end error states

--- src/app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ src/app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -14,7 +14,12 @@ import { Baggage } from '../../models/baggage.interface';
         <input
           type="text"
           name="fullname"
+          required
+          #fullname="ngModel"
           [ngModel]="detail?.fullname">
+        <div *ngIf="fullname.errors?.required $$ fullname.dirty" class="error">
+          Passenger name is required
+        </div>  
       </div>
       
       <div>
@@ -22,7 +27,12 @@ import { Baggage } from '../../models/baggage.interface';
         <input
           type="number"
           name="id"
+          required
+          #id="ngModel"
           [ngModel]="detail?.id">
+        <div *ngIf="id.errors?.required $$ id.dirty" class="error">
+          Passenger id is required
+        </div>
       </div>
 
       <div>
@@ -66,7 +76,9 @@ import { Baggage } from '../../models/baggage.interface';
         </select>
       </div>
       
-      {{form.value | json}}
+      <div>{{form.value | json}}</div>
+      <div>Valid: {{form.valid | json}}</div>
+      <div>Invalid: {{form.invalid | json}}</div>
     </form>
   `
 })
